1bb41d9562e09a6a144c9bf844e046bdcd923915
augustin-raynaud <augustin.raynaud@epitech.eu>
2023-01-04 00:37:14 +0100

    remove files

--- EscapeKart/Source/EscapeKart/VehiclePawn.cpp
+++ EscapeKart/Source/EscapeKart/VehiclePawn.cpp
@@ -6,169 +6,197 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
-#include "WheeledVehicleMovementComponent4W.h"
+#include "WheeledVehicleMovementcomponent4W.h"
+#include "Kismet/GameplayStatics.h"
 
-static const FName NAME_SteerInput("Steer");
-static const FName NAME_ThrottleInput("Throttle");
-
-AVehiclePawn::AVehiclePawn() {
+AVehiclePawn::AVehiclePawn()
+{
 	UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement());
 
-	// Adjust the tire loading
-	Vehicle4W->MinNormalizedTireLoad = 0.0f;
-	//Vehicle4W->MinNormalizedTireLoadFiltered = 0.2f;
-	//Vehicle4W->MaxNormalizedTireLoad = 2.0f;
-	//Vehicle4W->MaxNormalizedTireLoadFiltered = 2.0f;
-	Vehicle4W->MinNormalizedTireLoadFiltered = 2.0f;
-	Vehicle4W->MaxNormalizedTireLoad = 3.0f;
-	Vehicle4W->MaxNormalizedTireLoadFiltered = 3.0f;
+	// Adjust the tire
+	Vehicle4W->MinNormalizedTireLoad = MinTireLoad;
+	Vehicle4W->MinNormalizedTireLoadFiltered = MinTireLoadFiltered;
+	Vehicle4W->MaxNormalizedTireLoad = MaxTireLoad;
+	Vehicle4W->MaxNormalizedTireLoadFiltered = MaxTireLoadFiltered;
 
 	// Torque setup
-	Vehicle4W->MaxEngineRPM = 8000000.0f;
+	Vehicle4W->MaxEngineRPM = MaxEngineRPM;
 	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->Reset();
-	//Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 38.0f);
-	//Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(40.0f, 80.0f);
-	//Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(100.0f, 160.0f);
-	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 4000000.0f);
-	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.1f, 6000000.0f);
-	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.2f, 8000000.0f);
+	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(FirstTorqueMin, FirstTorqueMax);
+	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(SecondTorqueMin, SecondTorqueMax);
+	Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(ThirdTorqueMin, ThirdTorqueMax);
 
 	// Adjust the steering 
 	Vehicle4W->SteeringCurve.GetRichCurve()->Reset();
-	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(0.0f, 1.0f);
-	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(40.0f, 0.7f);
-	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(120.0f, 0.6f);
+	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(FirstSteeringMin, FirstSteeringMax);
+	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(SecondSteeringMin, SecondSteeringMax);
+	Vehicle4W->SteeringCurve.GetRichCurve()->AddKey(ThirdSteeringMin, ThirdSteeringMax);
 
 	Vehicle4W->DifferentialSetup.DifferentialType = EVehicleDifferential4W::LimitedSlip_4W;
-	Vehicle4W->DifferentialSetup.FrontRearSplit = 0;
+	Vehicle4W->DifferentialSetup.FrontRearSplit = 0.65f;
 
 	// Automatic gearbox
-	Vehicle4W->TransmissionSetup.bUseGearAutoBox = true;
-	Vehicle4W->TransmissionSetup.GearSwitchTime = 0;
-	Vehicle4W->TransmissionSetup.GearAutoBoxLatency = 0;
+	Vehicle4W->TransmissionSetup.bUseGearAutoBox = GearAutoBox;
+	Vehicle4W->TransmissionSetup.GearSwitchTime = GearSwitchTime;
+	Vehicle4W->TransmissionSetup.GearAutoBoxLatency = GearAutoBoxLatency;
 
-	// Create a spring arm component for our chase camera
+	// Spring arm
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(RootComponent);
-	SpringArm->TargetArmLength = 250.0f;
+	SpringArm->TargetArmLength = 300.0f;
 	SpringArm->bUsePawnControlRotation = true;
 
-	// Create the chase camera component
-	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("ChaseCamera"));
+	// Camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
-	Camera->FieldOfView = 110.0f;
+	Camera->FieldOfView = 90.0f;
 
 }
 
-void AVehiclePawn::Tick(float DeltaTime) {
+void AVehiclePawn::Tick(float DeltaTime)
+{
 	Super::Tick(DeltaTime);
 
-	UpdateInAirControl(DeltaTime);
+	//UpdateInAirControl(DeltaTime);
 }
 
-void AVehiclePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+void AVehiclePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis(NAME_ThrottleInput, this, &AVehiclePawn::ApplyThrottle);
-	PlayerInputComponent->BindAxis(NAME_SteerInput, this, &AVehiclePawn::ApplySteering);
-	PlayerInputComponent->BindAxis("LookUp", this, &AVehiclePawn::LookUp);
+	PlayerInputComponent->BindAxis("Throttle", this, &AVehiclePawn::ApplyThrottle);
+	PlayerInputComponent->BindAxis("Steer", this, &AVehiclePawn::ApplySteering);
+	PlayerInputComponent->BindAxis("LookUp", this, &AVehiclePawn::Lookup);
 	PlayerInputComponent->BindAxis("Turn", this, &AVehiclePawn::Turn);
 
-	PlayerInputComponent->BindAction("Handbrake", IE_Pressed, this, &AVehiclePawn::onHandbrakePressed);
-	PlayerInputComponent->BindAction("Handbrake", IE_Released, this, &AVehiclePawn::onHandbrakeReleased);
+	PlayerInputComponent->BindAction("Handbrake", IE_Pressed, this, &AVehiclePawn::OnHandbrakePressed);
+	PlayerInputComponent->BindAction("Handbrake", IE_Released, this, &AVehiclePawn::OnHandbrakeReleased);
 
-	PlayerInputComponent->BindAction("Accelerate", IE_Pressed, this, &AVehiclePawn::onAcceleratePressed);
-	PlayerInputComponent->BindAction("Accelerate", IE_Released, this, &AVehiclePawn::onAccelerateReleased);
+	PlayerInputComponent->BindAction("Accelerate", IE_Pressed, this, &AVehiclePawn::OnAcceleratePressed);
+	PlayerInputComponent->BindAction("Accelerate", IE_Released, this, &AVehiclePawn::OnAccelerateReleased);
 }
 
-void AVehiclePawn::ApplyThrottle(float Val) {
+void AVehiclePawn::ApplyThrottle(float Val)
+{
 	GetVehicleMovementComponent()->SetThrottleInput(Val);
 }
 
-void AVehiclePawn::ApplySteering(float Val) {
+void AVehiclePawn::ApplySteering(float Val)
+{
 	GetVehicleMovementComponent()->SetSteeringInput(Val);
 }
 
-void AVehiclePawn::LookUp(float Val) {
-	if (Val != 0.f) {
-		AddControllerPitchInput(-Val);
+void AVehiclePawn::Lookup(float Val)
+{
+	if (Val != 0.f)
+	{
+		AddControllerPitchInput(Val);
 	}
 }
 
-void AVehiclePawn::Turn(float Val) {
-	if (Val != 0.f) {
+void AVehiclePawn::Turn(float Val)
+{
+	if (Val != 0.f)
+	{
 		AddControllerYawInput(Val);
 	}
 }
 
-void AVehiclePawn::onHandbrakePressed() {
+void AVehiclePawn::OnHandbrakePressed()
+{
 	GetVehicleMovementComponent()->SetHandbrakeInput(true);
 }
 
-void AVehiclePawn::onHandbrakeReleased() {
+void AVehiclePawn::OnHandbrakeReleased()
+{
 	GetVehicleMovementComponent()->SetHandbrakeInput(false);
 }
 
-void AVehiclePawn::onAcceleratePressed() {
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Accelerate")));
-	if (UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement())) {
+void AVehiclePawn::OnAcceleratePressed()
+{
+	if (UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement()))
+	{
+		Camera->FieldOfView = 150.0f;
+	}
 
-		Vehicle4W->MinNormalizedTireLoadFiltered = 4.0f;
-		Vehicle4W->MaxNormalizedTireLoad = 5.0f;
-		Vehicle4W->MaxNormalizedTireLoadFiltered = 5.0f;
+}
 
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->Reset();
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 400.0f);
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(1.0f, 50000.0f);
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(2.0f, 400000.0f);
-
-		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Applied")));
+void AVehiclePawn::OnAccelerateReleased()
+{
+	if (UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement()))
+	{
+		Camera->FieldOfView = 90.0f;
 	}
 }
 
-void AVehiclePawn::onAccelerateReleased() {
-	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Stop Accelerate")));
-	if (UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement())) {
+void AVehiclePawn::setRespawnLocation(FVector LocToSave)
+{
+	RespawnLocation = LocToSave;
+}
 
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 38.0f);
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(40.0f, 80.0f);
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(100.0f, 160.0f);
+FVector AVehiclePawn::getRespawnLocation()
+{
+	return FVector(RespawnLocation);
+}
 
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->Reset();
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(0.0f, 8.0f);
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(10.0f, 18.0f);
-		Vehicle4W->EngineSetup.TorqueCurve.GetRichCurve()->AddKey(20.0f, 22.0f);
+void AVehiclePawn::increaseLap()
+{
+	currentLap++;
+	if (currentLap == 1)
+	{
+		FTimerHandle UnusedHandle;
+		GetWorldTimerManager().SetTimer(UnusedHandle, this, &AVehiclePawn::startTimer, 1, true);
+	}
+	if (currentLap == maxCurrentLap)
+	{
+		endGame();
 	}
 }
 
-void AVehiclePawn::UpdateInAirControl(float DeltaTime) {
-	if (UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement())) {
+int AVehiclePawn::getCurrentLap()
+{
+	return currentLap;
+}
+
+void AVehiclePawn::endGame()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), FName("MainMenu"), true);
+}
+
+void AVehiclePawn::startTimer()
+{
+	seconds++;
+}
+
+void AVehiclePawn::UpdateInAirControl(float DeltaTime)
+{
+	if (UWheeledVehicleMovementComponent4W* Vehicle4W = CastChecked<UWheeledVehicleMovementComponent4W>(GetVehicleMovement()))
+	{
 		FCollisionQueryParams QueryParams;
 		QueryParams.AddIgnoredActor(this);
 
-		const FVector TraceStart = GetActorLocation() + FVector(0.f, 0.f, 50.f);
+		const FVector TraceStart = GetActorLocation() + FVector(0.f, 0.f, 50.0f);
 		const FVector TraceEnd = GetActorLocation() - FVector(0.f, 0.f, 200.f);
 
 		FHitResult Hit;
 
-		// Check if car is flipped on its side and check if the car is in the air
 		const bool bInAir = !GetWorld()->LineTraceSingleByChannel(Hit, TraceStart, TraceEnd, ECC_Visibility, QueryParams);
 		const bool bNotGrounded = FVector::DotProduct(GetActorUpVector(), FVector::UpVector) < 0.1f;
 
-		// Only allow in air-movement if we are not on the ground, or if we are in the air
-		if (bInAir || bNotGrounded) {
-			const float ForwardInput = InputComponent->GetAxisValue(NAME_ThrottleInput);
-			const float RightInput = InputComponent->GetAxisValue(NAME_SteerInput);
+		if (bInAir || bNotGrounded)
+		{
+			const float ForwardInput = InputComponent->GetAxisValue("Throttle");
+			const float RightInput = InputComponent->GetAxisValue("Steer");
 
-			// If car is grounded, allow player to roll the car over
-			const float AirMovementForcePitch = 1.5f;
-			const float AirMovementForceRoll = !bInAir && bNotGrounded ? 20.f : 1.5f;
+			const float AirMovementForcePitch = 3.f;
+			const float AirMovementForceRoll = !bInAir && bNotGrounded ? 20.f : 3.f; // in air = 3; ground = 20
 
-			if (UPrimitiveComponent* vehicleMesh = Vehicle4W->UpdatedPrimitive) {
-				const FVector MovementVector = FVector(RightInput * -AirMovementForceRoll, ForwardInput * AirMovementForcePitch, 0.f) * DeltaTime * 200.f;
+			if (UPrimitiveComponent* VehicleMesh = Vehicle4W->UpdatedPrimitive)
+			{
+				const FVector MovementVector = FVector(RightInput * -AirMovementForceRoll, ForwardInput * AirMovementForcePitch, 0.f);
 				const FVector NewAngularMovement = GetActorRotation().RotateVector(MovementVector);
 
-				vehicleMesh->SetPhysicsAngularVelocity(NewAngularMovement, true);
+				VehicleMesh->SetPhysicsAngularVelocity(NewAngularMovement, true);
 			}
 		}
 	}
--- EscapeKart/Source/EscapeKart/VehiclePawn.h
+++ EscapeKart/Source/EscapeKart/VehiclePawn.h
@@ -7,44 +7,118 @@
 #include "VehiclePawn.generated.h"
 
 /**
- *
+ * 
  */
 UCLASS()
 class ESCAPEKART_API AVehiclePawn : public AWheeledVehicle
 {
 	GENERATED_BODY()
-
+	
 public:
 	AVehiclePawn();
 
-	virtual void Tick(float DeltaTime) override;
+	virtual void Tick(float DeltaTime) override; // Function that is called every frame
 
 	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
-	/*Throttle/Steering*/
-	void ApplyThrottle(float Val);
+	// Throttle + Steering
+	void ApplyThrottle(float Val); // Val = how much force we want to apply
 	void ApplySteering(float Val);
 
-	/*Look around*/
-	void LookUp(float Val);
+	// Camera mouse input
+	void Lookup(float Val);
 	void Turn(float Val);
 
-	/*Handbrake*/
-	void onHandbrakePressed();
-	void onHandbrakeReleased();
+	// Handbrake
+	void OnHandbrakePressed();
+	void OnHandbrakeReleased();
 
-	/*Accelerate*/
-	void onAcceleratePressed();
-	void onAccelerateReleased();
+	// Accelerate
+	void OnAcceleratePressed();
+	void OnAccelerateReleased();
 
-	/*Update in air physics*/
+	// Respawn
+	void setRespawnLocation(FVector LocToSave);
+	FVector getRespawnLocation();
+
+	// Lap
+	void increaseLap();
+	UFUNCTION(Category = Race, BlueprintCallable, BlueprintPure)
+		int getCurrentLap();
+	UPROPERTY(Category = Race, EditAnywhere, BlueprintReadOnly)
+		int maxCurrentLap = 3;
+
+	// EndGame + Timer
+	void endGame();
+	void startTimer();
+
+	UPROPERTY(Category = Timer, EditDefaultsOnly, BlueprintReadOnly)
+		int seconds;
+
+
+
+	// Tire variables
+	UPROPERTY(Category = Tire, EditDefaultsOnly, BlueprintReadOnly)
+	float MinTireLoad = 0.0f;
+	UPROPERTY(Category = Tire, EditDefaultsOnly, BlueprintReadOnly)
+	float MinTireLoadFiltered = 0.2f;
+	UPROPERTY(Category = Tire, EditDefaultsOnly, BlueprintReadOnly)
+	float MaxTireLoad = 2.0f;
+	UPROPERTY(Category = Tire, EditDefaultsOnly, BlueprintReadOnly)
+	float MaxTireLoadFiltered = 2.0f;
+
+	// Torque variables
+	UPROPERTY(Category = Torque, EditDefaultsOnly, BlueprintReadOnly)
+		float MaxEngineRPM = 5700.0f;
+	UPROPERTY(Category = Torque, EditDefaultsOnly, BlueprintReadOnly)
+		float FirstTorqueMin = 0.0f;
+	UPROPERTY(Category = Torque, EditDefaultsOnly, BlueprintReadOnly)
+		float FirstTorqueMax = 400.0f;
+	UPROPERTY(Category = Torque, EditDefaultsOnly, BlueprintReadOnly)
+		float SecondTorqueMin = 1890.0f;
+	UPROPERTY(Category = Torque, EditDefaultsOnly, BlueprintReadOnly)
+		float SecondTorqueMax = 500.0f;
+	UPROPERTY(Category = Torque, EditDefaultsOnly, BlueprintReadOnly)
+		float ThirdTorqueMin = 5730.0f;
+	UPROPERTY(Category = Torque, EditDefaultsOnly, BlueprintReadOnly)
+		float ThirdTorqueMax = 400.0f;
+
+	// Steering variables
+	UPROPERTY(Category = Sterring, EditDefaultsOnly, BlueprintReadOnly)
+		float FirstSteeringMin = 0.0f;
+	UPROPERTY(Category = Sterring, EditDefaultsOnly, BlueprintReadOnly)
+		float FirstSteeringMax = 1.0f;
+	UPROPERTY(Category = Sterring, EditDefaultsOnly, BlueprintReadOnly)
+		float SecondSteeringMin = 40.0f;
+	UPROPERTY(Category = Sterring, EditDefaultsOnly, BlueprintReadOnly)
+		float SecondSteeringMax = 0.7f;
+	UPROPERTY(Category = Sterring, EditDefaultsOnly, BlueprintReadOnly)
+		float ThirdSteeringMin = 120.0f;
+	UPROPERTY(Category = Sterring, EditDefaultsOnly, BlueprintReadOnly)
+		float ThirdSteeringMax = 0.6f;
+
+	// Gearbox variables
+	UPROPERTY(Category = Gearbox, EditDefaultsOnly, BlueprintReadOnly)
+		bool GearAutoBox = true;
+	UPROPERTY(Category = Gearbox, EditDefaultsOnly, BlueprintReadOnly)
+		float GearSwitchTime = 0.15f;
+	UPROPERTY(Category = Gearbox, EditDefaultsOnly, BlueprintReadOnly)
+		float GearAutoBoxLatency = 1.0f;
+
+	// Air Physics
 	void UpdateInAirControl(float DeltaTime);
 
 protected:
-	/*Spring arm that will offset the camera*/
+
+	// Spring arm for the camera
 	UPROPERTY(Category = Camera, EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 		class USpringArmComponent* SpringArm;
 
+	// Camera
 	UPROPERTY(Category = Camera, EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 		class UCameraComponent* Camera;
+
+private:
+	FVector RespawnLocation;
+	int currentLap = 0;
 };
